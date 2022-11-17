d9d53bd2c6bee2a65c654e79e83f43205395db64
JarekSopinski <jarek.sopinski@gmail.com>
2022-11-17 11:52:35 +0100

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { firebaseApiKey } from './config-secret';
+
+const firebaseConfig = {
+    apiKey: firebaseApiKey,
+    authDomain: "n22-thedojo.firebaseapp.com",
+    projectId: "n22-thedojo",
+    storageBucket: "n22-thedojo.appspot.com",
+    messagingSenderId: "40756640888",
+    appId: "1:40756640888:web:7d26400d1fd7511828418d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
