bbe41ebf07b2aae1409178f1916ca3908c85619a
JarekSopinski <jarek.sopinski@gmail.com>
2022-11-18 15:10:34 +0100

    Firebase Storage Setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 import { firebaseApiKey } from './config-secret';
 
 const firebaseConfig = {
@@ -16,7 +17,8 @@ firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
