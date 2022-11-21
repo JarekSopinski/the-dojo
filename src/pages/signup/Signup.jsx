1f248bc9543c401aaddddd68da128480cda214db
JarekSopinski <jarek.sopinski@gmail.com>
2022-11-21 14:35:48 +0100

    Signing a User Up

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSignup } from '../../hooks/useSignup';
 import './Signup.css';
 
 export default function Signup() {
@@ -9,9 +10,11 @@ export default function Signup() {
   const [thumbnail, setThumbnail] = useState(null);
   const [thumbnailError, setThumbnailError] = useState(null);
 
+  const { signup, isPending, error } = useSignup();
+
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(email, password, displayName, thumbnail);
+    signup(email, password, displayName, thumbnail);
   };
 
   const handleFileChange = e => {
@@ -81,7 +84,10 @@ export default function Signup() {
         {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
 
-      <button className='btn'>Sign up</button>
+      {!isPending && <button className='btn'>Sign up</button>}
+      {isPending && <button className='btn' disabled>Loading</button>}
+
+      {error && <div className='error'>{error}</div>}
 
     </form>
   );
