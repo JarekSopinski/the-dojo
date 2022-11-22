e575dc81e18d564fc3bbadebf41b71e2fc454bb5
JarekSopinski <jarek.sopinski@gmail.com>
2022-11-22 08:55:36 +0100

    Making a Login Page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
 import './Login.css';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { isPending, error } = useLogin();
+
+  const handleLogin = e => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>Login</div>
+    <form className='auth-form' onSubmit={handleLogin}>
+      <h2>Login</h2>
+
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      {!isPending && <button className='btn'>Login</button>}
+      {isPending && <button className='btn' disabled>Loading</button>}
+
+      {error && <div className='error'>{error}</div>}
+
+    </form>
   );
 };
